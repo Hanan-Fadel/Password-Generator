cb71b7d29b85e5a1d1c5ed0a3f19b1f075633d75
Hanan Fadel <hfadel@ellerca.com>
2022-01-31 09:23:19 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  password = '';
+  length = 0;
+  includeLetters = false;
+  includesNumbers = false;
+  includesSymbols = false;
+
+  onButtonClick() {
+    const numbers = "1234567890";
+    const letters = "abcdefghijklmnopqrstuvwz";
+    const symbols = "!@#$%^&*()";
+    let validChars ='';
+
+    if(this.includeLetters) {
+      validChars +=letters;
+    }
+
+    if(this.includesNumbers) {
+      validChars +=numbers;
+    }
+
+    if(this.includesSymbols) {
+      validChars +=symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i=0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword+=validChars[index];
+    }
+
+    this.password = generatedPassword;
+
+    // console.log(`
+    //   About to generate a password with the following:
+    //   Length: ${this.length}
+    //   Includes letters: ${this.includeLetters}
+    //   Includes numbers: ${this.includesNumbers}
+    //   Includes Symbols: ${this.includesSymbols}
+    // `);
+
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    //Check if the user entered a number or non isNaN returns false if the user entered a number
+    if(!isNaN(parsedValue)) {
+      //The entered value is number
+      this.length = parsedValue;
+    }
+  }
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includesNumbers = !this.includesNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includesSymbols = !this.includesSymbols;
+  }
 }
